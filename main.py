935e3211784c8f188e4eab7dd1dd0a17d0a52f14
Alankar Jain <alankarj@andrew.cmu.edu>
2019-11-29 10:17:50 -0800

    commented preprocessing code

--- main.py
+++ main.py
@@ -9,25 +9,6 @@ import pickle
 random.seed(config.SEED)
 
 
-def verify_arguments(args):
-    if args.src_lang not in config.SRC_LANGS:
-        raise ValueError(val_error_msg(args.src_lang, 'src_lang') % args.src_lang)
-    if args.tgt_lang not in config.TGT_LANGS:
-        raise ValueError(val_error_msg(args.tgt_lang, 'tgt_lang') % args.tgt_lang)
-    # if args.translate_fname not in config.TRANSLATE_FNAMES:
-    #     raise ValueError(val_error_msg(args.translate_fname, 'translate_fname') % args.translate_fname)
-    if args.pre_process not in [0, 1]:
-        raise ValueError(val_error_msg(args.pre_process, 'pre_process') % args.pre_process)
-    if args.trans_sent not in [0, 1, 2]:
-        raise ValueError(val_error_msg(args.trans_sent, 'trans_sent') % args.trans_sent)
-    if args.translate_all not in [0, 1]:
-        raise ValueError(val_error_msg(args.translate_all, 'translate_all') % args.translate_all)
-    if args.verbosity not in [0, 1, 2]:
-        raise ValueError(val_error_msg(args.verbosity, 'verbosity') % args.verbosity)
-    if args.num_sample < 0:
-        raise ValueError(val_error_msg(args.num_sample, 'num_sample') % args.num_sample)
-
-
 def val_error_msg(var, name):
     assert type(name) == str
     if type(var) == int:
@@ -69,26 +50,64 @@ def get_annotated_list(file_path):
 
 
 def parse_arguments():
-    parser = ArgumentParser(description='Argument Parser for cross-lingual NER.')
-
-    parser.add_argument('--src_lang', dest='src_lang', type=str, default='en', help='Source language')
-    parser.add_argument('--tgt_lang', dest='tgt_lang', type=str, default='es', help='Target language')
-    parser.add_argument('--translate_fname', dest='translate_fname', type=str, default='train',
-                        help='File name to be translated from src_lang to tgt_lang (train, dev, test)')
-    parser.add_argument('--api_key_fname', dest='api_key_fname', type=str, default='api_key',
-                        help='File name for Google API key')
-    parser.add_argument('--pre_process', dest='pre_process', type=int, default=0,
-                        help='Whether to pre-process raw data or not')
-    parser.add_argument('--trans_sent', dest='trans_sent', type=int, default=0,
-                        help='Whether to translate full sentences or not')
-    parser.add_argument('--translate_all', dest='translate_all', type=int, default=1,
-                        help='Whether to translate all sentences or not')
-    parser.add_argument('--verbosity', dest='verbosity', type=int, default=2, help='Verbosity level')
-    parser.add_argument('--sentence_ids_file', dest='sentence_ids_file', type=str, default="sentence_ids",
-                        help='Sentence IDs file name')
-    parser.add_argument('--num_sample', dest='num_sample', type=int, default=100,
-                        help='Number of sentences to sample for partial translation.')
+    parser = ArgumentParser(description="Argument Parser for cross-lingual "
+                                        "NER.")
+    parser.add_argument("--src_lang", dest="src_lang", type=str, default="en",
+                        help="Source language")
+    parser.add_argument("--tgt_lang", dest="tgt_lang", type=str, default="es",
+                        help='Target language')
+    parser.add_argument("--translate_fname", dest="translate_fname", type=str,
+                        default="train",
+                        help="File name to be translated from src_lang to "
+                             "tgt_lang (train, dev, test)")
+    parser.add_argument("--api_key_fname", dest="api_key_fname", type=str,
+                        default="api_key", help="File name for Google API key")
+    # Set this flag to 1 if the input is in its raw form, i.e., in a text file
+    # with each token and its corresponding tag (separated by a space) in
+    # a separate line.
+    parser.add_argument("--pre_process", dest="pre_process", type=int,
+                        default=0,
+                        help="Whether to pre-process raw data or not")
+    # Set this flag to 1 if the TRANSLATE step, i.e., translation of the
+    # annotated source corpus to the target language, sentence-by-sentence needs
+    # to be performed. Set it to 2 if only the first step of MATCH,
+    # i.e., translation of entity phrases needs to be performed.
+    parser.add_argument("--trans_sent", dest="trans_sent", type=int, default=0,
+                        help="Whether to translate full sentences or not")
+    # TODO(alankarjain): Explain what translating all sentences means.
+    parser.add_argument("--translate_all", dest="translate_all", type=int,
+                        default=1,
+                        help="Whether to translate all sentences or not")
+    # Verbosity of the output (0/1/2).
+    parser.add_argument("--verbosity", dest="verbosity", type=int, default=2,
+                        help="Verbosity level")
+    # TODO(alankarjain): Explain what sentence_ids means.
+    parser.add_argument("--sentence_ids_file", dest="sentence_ids_file",
+                        type=str, default="sentence_ids",
+                        help="Sentence IDs file name")
+    # TODO(alankarjain): Explain what num_sample means.
+    parser.add_argument('--num_sample', dest='num_sample', type=int,
+                        default=100, help="Number of sentences to sample for "
+                                          "partial translation.")
     return parser.parse_args()
+
+
+def verify_arguments(args):
+    if args.pre_process not in [0, 1]:
+        raise ValueError(val_error_msg(args.pre_process, "pre_process") %
+                         args.pre_process)
+    if args.trans_sent not in [0, 1, 2]:
+        raise ValueError(val_error_msg(args.trans_sent, "trans_sent") %
+                         args.trans_sent)
+    if args.translate_all not in [0, 1]:
+        raise ValueError(val_error_msg(args.translate_all, "translate_all") %
+                         args.translate_all)
+    if args.verbosity not in [0, 1, 2]:
+        raise ValueError(val_error_msg(args.verbosity, "verbosity") %
+                         args.verbosity)
+    if args.num_sample < 0:
+        raise ValueError(val_error_msg(args.num_sample, "num_sample") %
+                         args.num_sample)
 
 
 def main():
@@ -100,11 +119,17 @@ def main():
     args.tgt_file_path = os.path.join(args.data_path, args.tgt_lang)
 
     file_name = args.translate_fname + "_processed"
-    annotated_list_file_path = os.path.join(args.src_file_path, file_name + ".pkl")
+    annotated_list_file_path = os.path.join(args.src_file_path,
+                                            file_name + ".pkl")
 
+    # Pre-process the raw input only if the flag is set to 1.
     if args.pre_process == 1:
         file_path = os.path.join(args.src_file_path, args.translate_fname)
-        annotated_data = data_processing.AnnotatedData(file_path, lang=None, verbosity=args.verbosity)
+        # Unless supported by config.gold_test_dict, set the lang always to
+        # None below. This argument is only used for performing tests to ensure
+        # that the preprocessing has been performed correctly.
+        annotated_data = data_processing.AnnotatedData(file_path, lang=None,
+                                                       verbosity=args.verbosity)
         annotated_list = annotated_data.process_data()
         with open(annotated_list_file_path, 'wb') as f:
             pickle.dump(annotated_list, f)
--- src/util/data_processing.py
+++ src/util/data_processing.py
@@ -5,26 +5,39 @@ import os
 
 
 class AnnotatedData:
-
+    """
+    Class for processing raw input data (tokens separated by tags on separate
+    lines) into a structured annotated form.
+    """
     def __init__(self, file_path, lang=None, verbosity=0):
         self.file_path = file_path
+        # Language of the input file.
         self.lang = lang
         self.verbosity = verbosity
+        # List of all annotated sentences.
         self.annotated_list = None
+        # Documents marked by "-DOCSTART-". This value is calculated and used
+        # only for testing, if the lang is set to a non-None value.
         self.num_docs = 0
 
     def process_data(self):
-        with open(self.file_path, mode='r', encoding='utf-8') as f:
+        with open(self.file_path, mode="r", encoding="utf-8") as f:
             all_rows = f.readlines()
+            # List of all annotated sentences in the input corpus.
             annotated_list = list()
+            # Each sentence is stored as a list of tuples (that contain token
+            # as their first element and the tag as the last).
             sentence = list()
 
             for i, row in enumerate(all_rows):
                 row = row.rstrip("\n").split(" ")
                 if row[0].startswith(config.DOC_BEGIN_STR):
                     self.num_docs += 1
+                    # No need to parse this line.
                     continue
-                if row == ['']:
+                if row == [""]:
+                    # This indicates the beginning of a new sentence. Hence,
+                    # annotate the sentence found so far.
                     a = self._create_annotation(sentence)
                     if a is not None:
                         annotated_list.append(a)
@@ -37,7 +50,8 @@ class AnnotatedData:
                 annotated_list.append(a)
 
             if self.verbosity >= 1:
-                print("Number of documents: ", self.num_docs)
+                if self.lang is not None:
+                    print("Number of documents: ", self.num_docs)
                 print("Number of annotated sentences: ", len(annotated_list))
             self.annotated_list = annotated_list
             if self.lang is not None:
@@ -45,12 +59,19 @@ class AnnotatedData:
             return annotated_list
 
     def _create_annotation(self, sentence):
+        """
+        :param sentence: List of tuples (that contain token as their first
+        element and the tag as the last).
+        :return: Nothing if the sentence isn't an empty list. Else, return an
+        Annotation object corresponding to the sentence.
+        """
         sentence = list(map(list, zip(*sentence)))
-        if sentence != list():
+        if sentence:
             a = Annotation()
             a.tokens = sentence[0]
             a.ner_tags = sentence[-1]
-            a.span_list = get_entity_spans(a.ner_tags)
+            # Get Span objects for all entities in the sentence.
+            a.span_list = _get_entity_spans(a.ner_tags)
             if self.verbosity == 2:
                 print("Sentences: ", a.tokens)
                 print("NER Tags: ", a.ner_tags)
@@ -61,8 +82,16 @@ class AnnotatedData:
         return None
 
     def _processing_test(self):
+        """
+        Perform a series of checks to test whether the input file has been
+        correctly parsed. Works only for languages for which gold counts exist
+        in config.gold_test_dict exists.
+        :return: Nothing.
+        """
+        assert self.lang in config.gold_test_dict
         if self.num_docs > 0:
-            assert self.num_docs == config.gold_test_dict[self.lang]["documents"]
+            assert self.num_docs == \
+                   config.gold_test_dict[self.lang]["documents"]
         test_dict = dict()
         test_dict["sentences"] = len(self.annotated_list)
         test_dict["tokens"] = 0
@@ -76,26 +105,42 @@ class AnnotatedData:
         for key in test_dict:
             if self.verbosity == 2:
                 print("Key: ", key, end='')
-                print(", Test number: ", test_dict[key], end='')
-                print(", Gold number: ", config.gold_test_dict[self.lang][key])
+                print(", Test count: ", test_dict[key], end='')
+                print(", Gold count: ", config.gold_test_dict[self.lang][key])
             assert test_dict[key] == config.gold_test_dict[self.lang][key]
 
 
 class Annotation:
+    """
+    Class for an annotated sentence.
+    """
     def __init__(self):
+        # List of tokens in the sentence.
         self.tokens = list()
+        # List of NER tags in the sentence, one corresponding to each token.
         self.ner_tags = list()
+        # A list of spans of all the entity phrases in the sentence.
         self.span_list = list()
 
 
 class Span:
+    """
+    Class for spans of entity phrases in a sentence.
+    """
     def __init__(self, beg, end, tag_type):
+        # Begin token index of the entity span
         self.beg = beg
+        # End token index of the entity span
         self.end = end
+        # Type of tag of the entity span
         self.tag_type = tag_type
 
 
-def get_entity_spans(ner_tags):
+def _get_entity_spans(ner_tags):
+    """
+    :param ner_tags: List of NER Tags in BIO format.
+    :return: List of span objects corresponding to distinct entity phrases.
+    """
     ner_tags = copy.deepcopy(ner_tags)
     ner_tags.append("O")
     span_list = list()
@@ -104,14 +149,15 @@ def get_entity_spans(ner_tags):
     for i, curr_tag in enumerate(ner_tags):
         curr_tag_type = curr_tag.split("-")[1] if curr_tag != "O" else None
         if not curr_tag.startswith("I"):
-            span_list = add_span(span_list, beg, i, prev_tag_type)
+            # This means that the previous span has ended, so we add it.
+            span_list = _add_span(span_list, beg, i, prev_tag_type)
             if curr_tag.startswith("B"):
                 beg = i
         prev_tag_type = curr_tag_type
     return span_list
 
 
-def add_span(span_list, beg, end, tag_type):
+def _add_span(span_list, beg, end, tag_type):
     if tag_type is not None:
         span_list.append(Span(beg, end, tag_type))
     return span_list
